eadecb9c24400175098a9e5f8aa1207054d42f12
Diara-Bamba <diarabamba@gmail.com>
2018-10-05 13:01:21 -0400

    Added FontAwesome via NPM

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 import {
     Button,
     Card,
+    Badge,
     CardBody,
     CardDeck,
+    CardFooter,
     CardImg, 
+    CardLink,
     CardSubtitle,
     CardText,
     CardTitle,
@@ -24,18 +27,28 @@ import {
     Jumbotron,
 } from 'reactstrap';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+
 class App extends Component {
   render() {
     return (
     <CardDeck>
-      <Card>
+      <Card body className="text-center">
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image" />
         <CardBody>
-          <CardTitle>test</CardTitle>
-          <CardSubtitle>test</CardSubtitle>
+          <CardTitle>Name</CardTitle>
+          <CardSubtitle className="text-muted">Company</CardSubtitle>
+          <Badge color="secondary">Title</Badge>
           <CardText>test</CardText>
           <Button>More</Button>
         </CardBody>
+        <CardFooter>
+          <Badge color="primary"><CardLink className="text-muted" href="#">Industry</CardLink></Badge>
+        </CardFooter>
       </Card>
     </CardDeck>
     );
